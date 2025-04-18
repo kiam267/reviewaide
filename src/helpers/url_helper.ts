209b58d9987ebd9bd71ec91b046b5d110199e7f2
Abid Hasan Kiam <kiamhasan267@gmail.com>
2025-04-18 18:58:20 +0600

    feat(server link): add a server link

--- src/helpers/url_helper.ts
+++ src/helpers/url_helper.ts
@@ -2,15 +2,25 @@
 
 // export const REACT_APP_SERVER_API = 'http://localhost:4500';
 // export const REVIEW_LINK = 'http://localhost:300'
-export const REVIEW_LINK = 'https://reviewaide.com'
-export const REACT_APP_SERVER_API = 'https://reviewaide.com';
+// export const REVIEW_LINK = 'http://localhost:4500';
+// export const REACT_APP_SERVER_API = 'http://localhost:4500';
 
 
-export const LINK = 'https://reviewaide.com/';
-export const AVATER_IMAGE_URL = 'https://reviewaide.com/';
-export const RESETPASSWORD_LINK = 'https://reviewaide.com';
-export const REVIEW_LOGO_LINK = 'https://reviewaide.com/';
-export const SERVER_LINK = 'https://reviewaide.com/';
+// export const LINK = 'http://localhost:4500/';
+// export const AVATER_IMAGE_URL = 'http://localhost:4500/';
+// export const RESETPASSWORD_LINK = 'http://localhost:4500';
+// export const REVIEW_LOGO_LINK = 'http://localhost:4500/';
+// export const SERVER_LINK = 'http://localhost:4500/';
+
+export const REVIEW_LINK = 'https://api-reviewaide.shariarkobirkiam.xyz';
+export const REACT_APP_SERVER_API = 'https://api-reviewaide.shariarkobirkiam.xyz';
+
+
+export const LINK = 'https://api-reviewaide.shariarkobirkiam.xyz/';
+export const AVATER_IMAGE_URL = 'https://api-reviewaide.shariarkobirkiam.xyz/';
+export const RESETPASSWORD_LINK = 'https://api-reviewaide.shariarkobirkiam.xyz';
+export const REVIEW_LOGO_LINK = 'https://api-reviewaide.shariarkobirkiam.xyz/';
+export const SERVER_LINK = 'https://api-reviewaide.shariarkobirkiam.xyz/';
 
 
 
